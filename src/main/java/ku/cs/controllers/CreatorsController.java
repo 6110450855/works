78c60ba50aaf9a4a81038033efb047739f0f47b0
6110450855 <pimpisut.pu@ku.th>
2021-12-28 17:11:07 +0700

    create Creator Page : Pimpisut

---
+++ src/main/java/ku/cs/controllers/CreatorsController.java
@@ -0,0 +1,26 @@
+package ku.cs.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import ku.cs.App;
+
+import java.io.IOException;
+
+public class CreatorsController {
+    @FXML private ImageView image1, image2, image3;
+    @FXML private Button MainPageButton;
+
+    @FXML
+    public void initialize() {
+        image1.setImage(new Image("/ku/cs/creators/6110450855.jpg"));
+        image2.setImage(new Image("/ku/cs/creators/6210451306.jpg"));
+        image3.setImage(new Image("/ku/cs/creators/6210451641.jpg"));
+    }
+
+    @FXML
+    public void goToMainPageButton() throws IOException {
+        App.setRoot("mainpage");
+    }
+}
